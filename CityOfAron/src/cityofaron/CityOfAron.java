cb75277681b3d2dfb14b3aba5710c2a480fb115f
glaucio <glaucio@DESKTOP-M797VA7>
2018-02-01 23:07:46 -0200

    alterações

--- CityOfAron/src/cityofaron/CityOfAron.java
+++ CityOfAron/src/cityofaron/CityOfAron.java
@@ -1,5 +1,6 @@
 package cityofaron;
 
+import cityofaron.model.CropData;
 import cityofaron.model.Game;
 import cityofaron.model.Map;
 import cityofaron.model.Player;
@@ -12,8 +13,12 @@ import cityofaron.model.Player;
 public class CityOfAron {
     public static void main(String[] args) {
         Game game = new Game();
-     
-       
+                
+        //creation of cropdata
+        CropData cropData = new CropData();
+        cropData.setYear(2018);
+        cropData.setPopulation(1000);
+        
         //creation of player1
         Player player1 = new Player();
         player1.setName("Joãozinho da Silva");
@@ -27,11 +32,12 @@ public class CityOfAron {
         //assigning the play and the map to the game...
         game.setThePlayer(player1);
         game.setTheMap(map);
-        
+        game.setCropData(cropData);
         
         
         //printing out the classes values        
         System.out.println(game.toString());
+        System.out.println(cropData.toString());
         System.out.println(map.toString());
         System.out.println(player1.toString());        
     }       
--- CityOfAron/src/cityofaron/model/Game.java
+++ CityOfAron/src/cityofaron/model/Game.java
@@ -36,4 +36,52 @@ public class Game implements Serializable{
     public void setThePlayer(Player thePlayer) {
         this.thePlayer = thePlayer;
     }
+
+    public CropData getCropData() {
+        return cropData;
+    }
+
+    public void setCropData(CropData cropData) {
+        this.cropData = cropData;
+    }
+
+    public TeamMember[] getTeam() {
+        return team;
+    }
+
+    public void setTeam(TeamMember[] team) {
+        this.team = team;
+    }
+
+    public ArrayList<ListItem> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(ArrayList<ListItem> animals) {
+        this.animals = animals;
+    }
+
+    public ArrayList<ListItem> getTools() {
+        return tools;
+    }
+
+    public void setTools(ArrayList<ListItem> tools) {
+        this.tools = tools;
+    }
+
+    public ListItem getProvisions() {
+        return provisions;
+    }
+
+    public void setProvisions(ListItem provisions) {
+        this.provisions = provisions;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "theMap=" + theMap + ", thePlayer=" + thePlayer + ", cropData=" + cropData + ", team=" + team + ", animals=" + animals + ", tools=" + tools + ", provisions=" + provisions + '}';
+    }
+    
+    
+    
 }
