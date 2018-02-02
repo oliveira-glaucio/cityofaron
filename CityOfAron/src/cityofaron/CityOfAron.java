4194905a9ac62435e5e6a95051d87f7fa6f9e477
glaucio <glaucio@DESKTOP-M797VA7>
2018-02-01 23:40:40 -0200

    alterações na classe Game

--- CityOfAron/src/cityofaron/CityOfAron.java
+++ CityOfAron/src/cityofaron/CityOfAron.java
@@ -2,8 +2,11 @@ package cityofaron;
 
 import cityofaron.model.CropData;
 import cityofaron.model.Game;
+import cityofaron.model.ListItem;
 import cityofaron.model.Map;
 import cityofaron.model.Player;
+import cityofaron.model.TeamMember;
+import java.util.ArrayList;
 
 
 /**
@@ -13,6 +16,16 @@ import cityofaron.model.Player;
 public class CityOfAron {
     public static void main(String[] args) {
         Game game = new Game();
+        
+        //Team Members
+        TeamMember[] teamMembers = new TeamMember[2];
+
+        teamMembers[0].setName("Mariazinha");
+        teamMembers[0].setTitle("Domestica");
+        
+        teamMembers[1].setName("Pedrinho");
+        teamMembers[1].setTitle("Lavrador");
+        
                 
         //creation of cropdata
         CropData cropData = new CropData();
@@ -29,15 +42,32 @@ public class CityOfAron {
         map.setRowCount(200);
         
         
+        //Defining Animals
+        ArrayList<ListItem> animals = new ArrayList<ListItem>();
+        animals.add(new ListItem("Dog",1));
+        animals.add(new ListItem("Cat",2));       
+                
+        //Defining Tools
+        ArrayList<ListItem> tools = new ArrayList<ListItem>();
+        tools.add(new ListItem("Axe",12));
+        tools.add(new ListItem("Shovel",2));       
+           
+        
+        
         //assigning the play and the map to the game...
         game.setThePlayer(player1);
         game.setTheMap(map);
         game.setCropData(cropData);
-        
+        game.setTeam(teamMembers);
+        game.setAnimals(animals);
+        game.setTools(tools);
         
         //printing out the classes values        
         System.out.println(game.toString());
+        System.out.println(teamMembers.toString());
         System.out.println(cropData.toString());
+        System.out.println(animals.toString());
+        System.out.println(tools.toString());
         System.out.println(map.toString());
         System.out.println(player1.toString());        
     }       
