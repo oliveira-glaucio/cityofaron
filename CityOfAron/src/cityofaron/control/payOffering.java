19c13bc63c2dabe6d57e97b9e2b3cbc0b1f25edd
macbook <macbook@localhost>
2018-02-17 15:37:02 -0200

    PayOffering and plantCrops added.

---
+++ CityOfAron/src/cityofaron/control/payOffering.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cityofaron.control;
+
+/**
+ *
+ * @author macbook
+ */
+public class payOffering {
+    // The payOffering method.
+    // Purpose: to subtract the value paid from user's storage.
+    // Parameters: the percentage of harvest paid.
+    // Returns: '0' if it's a valid value OR '-1' if it's an invalid value
+    // Pre-conditions: Offering percentage must be between 0 and 100
+    
+    public static int payOffering(int wheatHarvested) {
+        //If payOffering < 0, then return -1
+        if(payOffering < 0)
+            return -1;
+        //If payOffering > 100, then return -1
+        if(payOffering > 100)
+            return -1;
+        //wheatInStore - offeringAmount
+        payOffering = wheatInStore - offeringAmount;
+
+}
+
+    
+
+}
