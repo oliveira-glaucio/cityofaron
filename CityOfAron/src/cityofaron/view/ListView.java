1d7ae4249050eb3bdaa918de9103e2d0134f1164
glaucio <glaucio@DESKTOP-M797VA7>
2018-04-02 23:22:09 -0300

    function to export list implemented;

--- CityOfAron/src/cityofaron/view/ListView.java
+++ CityOfAron/src/cityofaron/view/ListView.java
@@ -8,42 +8,43 @@ package cityofaron.view;
 import cityofaron.MenuView;
 import cityofaron.model.Game;
 import cityofaron.model.ListItem;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import cityofaron.control.GameControl;//week-12
+import java.util.Date;
 
 /**
  *
  * @author glaucio
  */
 public class ListView extends MenuView {
-    
-private static Game theGame = cityofaron.CityOfAron.getTheGame();
-    
-    public ListView(){
-        super("\n" +
-                   "**********************************\n" +
-                   "* CITY OF AARON:      LIST MENU  *\n" +
-                   "**********************************\n" +
-                   " 1 - View the development team\n" +
-                   " 2 - View a list of animals\n" +
-                   " 3 - View a list of tools\n" +
-                   " 4 - View a list of provisions\n" +
-                   " 5 - Save a Report of provisions\n" +
-                   " 6 - Save a Report of Animals\n" +
-                   " 7 - Return to the game menu\n"
-             , 7);
+
+    private static Game theGame = cityofaron.CityOfAron.getTheGame();
+
+    public ListView() {
+        super("\n"
+                + "**********************************\n"
+                + "* CITY OF AARON:      LIST MENU  *\n"
+                + "**********************************\n"
+                + " 1 - View the development team\n"
+                + " 2 - View a list of animals\n"
+                + " 3 - View a list of tools\n"
+                + " 4 - View a list of provisions\n"
+                + " 5 - Save a Report of provisions\n"
+                + " 6 - Save a Report of Animals\n"
+                + " 7 - Return to the game menu\n",
+                 7);
     }
-    
-    
+
     // The doAction method
     // Purpose: performs the selected action
     // Parameters: none
     // Returns: none
     // ===================================       
     @Override
-    public void doAction(int option)
-    {
-        switch(option) {
+    public void doAction(int option) {
+        switch (option) {
             case 1: // display the Devlopment Team
                 viewDevelopmentTeam();
                 break;
@@ -60,67 +61,89 @@ private static Game theGame = cityofaron.CityOfAron.getTheGame();
                 saveReportProvisions();
                 break;
             case 6: // save a report of animals
-                saveReportAnimals();
+                //saveReportAnimals();
                 break;
             case 7: // return to the game menu
                 break;
         }
     }
-    
-    
-    public void viewDevelopmentTeam()
-    {
+
+    public void viewDevelopmentTeam() {
         System.out.println("City of Aron\nA game brought to you by Arunas and Gláucio.\nHope you enjoy it!");
     }
 
-    
-    public void viewListOfAnimals()
-    {
-        System.out.println("List of Animals inside the game:\n\n");        
+    public void viewListOfAnimals() {
+        System.out.println("List of Animals inside the game:\n\n");
         ArrayList<ListItem> listOfAnimals = theGame.getAnimals();
-        
-        for(int i = 0; i < listOfAnimals.size(); i++ )
-        {
-            ListItem currentAnimal = listOfAnimals.get(i);  
+
+        for (int i = 0; i < listOfAnimals.size(); i++) {
+            ListItem currentAnimal = listOfAnimals.get(i);
             System.out.println("Animal: " + currentAnimal.getName() + ", Worth: " + currentAnimal.getNumber());
-        }    
+        }
     }
 
-
-    public void viewListOfTools()
-    {
-        System.out.println("List of Tools inside the game:\n\n");        
+    public void viewListOfTools() {
+        System.out.println("List of Tools inside the game:\n\n");
         ArrayList<ListItem> listOfTools = theGame.getTools();
-        
-        for(int i = 0; i < listOfTools.size(); i++ )
-        {
-            ListItem currentTool = listOfTools.get(i);  
+
+        for (int i = 0; i < listOfTools.size(); i++) {
+            ListItem currentTool = listOfTools.get(i);
             System.out.println("Tool: " + currentTool.getName() + ", Worth: " + currentTool.getNumber());
-        }       
+        }
     }
 
-
-    public void viewListOfProvisions()
-    {
-        System.out.println("List of Provisions inside the game:\n\n");        
+    public void viewListOfProvisions() {
+        System.out.println("List of Provisions inside the game:\n\n");
         ArrayList<ListItem> listOfProvisions = theGame.getProvisions();
-        
-        for(int i = 0; i < listOfProvisions.size(); i++ )
-        {
-            ListItem currentProvision = listOfProvisions.get(i);  
+
+        for (int i = 0; i < listOfProvisions.size(); i++) {
+            ListItem currentProvision = listOfProvisions.get(i);
             System.out.println("Provision: " + currentProvision.getName() + ", Worth: " + currentProvision.getNumber());
-        }      
+        }
     }
 
-    private void saveReportProvisions() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void saveReportProvisions(){
+        String reportName;
+        PrintWriter pWriter = null;
+        
+        keyboard.nextLine();        
+        System.out.println("\nPlease enter of the file to save the report: ");
+        
+        reportName = keyboard.nextLine();
+
+        try{
+        pWriter = new PrintWriter(reportName);
+        
+        Date now = new Date();
+        pWriter.println("Provisions - Report - Date of Creation: " + now.toString());
+        
+        ArrayList<ListItem> listOfProvisions = theGame.getProvisions();
+
+        for (int i = 0; i < listOfProvisions.size(); i++) {
+            ListItem currentProvision = listOfProvisions.get(i);
+            pWriter.println("Provision: " + currentProvision.getName() + ", Worth: " + currentProvision.getNumber());
+        }
+        
+        pWriter.close();
+        
+        System.out.println("\nReport with provisions saved!");        
+        }
+        catch(IOException ex)
+        {
+            System.out.println("I/O Error: " + ex.getMessage());
+        }
+        finally{
+            if(pWriter != null) pWriter.close();
+        }
     }
 
-      private void saveReportAnimals() {
-      String saveAnimals;
-      System.out.println("\nPlease enther the name of your saved file.");
-      keyboard.nextLine();
-      saveAnimals = keyboard.nextLine();
-      GameControl.saveReportAnimals(saveAnimals);
-    }
+    //Please check if this is ok?
+    /*private void saveReportAnimals() {
+        String saveAnimals;
+        System.out.println("\nPlease enther the name of your saved file.");
+        keyboard.nextLine();
+        saveAnimals = keyboard.nextLine();
+        GameControl.saveReportAnimals(saveAnimal);
+    }*/
+
 }
